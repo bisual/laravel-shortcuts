b649db51676bf0b3f4401386bacbefcb2e757d6b
Ivan <ivanaguilarni@gmail.com>
2023-04-20 11:49:20 +0200

    add helpers class

---
+++ src/Helpers.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Bisual\LaravelShortcuts;
+
+class Helper
+{
+    public static function replaceTextVariablesByRegex(string $template, string $pattern, string $model) {
+        preg_match_all($pattern, $template, $values);
+        $variables_to_replace = $values[1];
+
+        return preg_replace_callback($pattern, function ($matches) use ($variables_to_replace, $model) {
+            $variable = $matches[1];
+            if (in_array($variable, $variables_to_replace)) {
+                try {
+                    $s = $model::show($variable)->value;
+                } catch (\Exception $e) {
+                    $s = "";
+                }
+                return $s;
+            }
+            return $matches[0];
+        }, $template);
+
+        return $template;
+    }
+}
