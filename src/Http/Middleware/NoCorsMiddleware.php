46067b4bdc3ea0685fb2152319755cd1b2900bdc
polribas14 <polribas14@users.noreply.github.com>
2023-04-25 14:01:40 +0000

    Fix styling

--- src/Http/Middleware/NoCorsMiddleware.php
+++ src/Http/Middleware/NoCorsMiddleware.php
@@ -4,14 +4,16 @@ namespace Bisual\LaravelShortcuts\Http\Middleware;
 
 use Closure;
 
-class NoCorsMiddleware {
-    public function handle($request, Closure $next) {
+class NoCorsMiddleware
+{
+    public function handle($request, Closure $next)
+    {
         $headers = [
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+            'Access-Control-Max-Age' => '86400',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With',
         ];
 
         if ($request->isMethod('OPTIONS')) {
@@ -20,11 +22,11 @@ class NoCorsMiddleware {
 
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', "*");
-        $response->headers->set('Access-Control-Allow-Methods', "POST, GET, OPTIONS, PUT, DELETE");
-        $response->headers->set('Access-Control-Allow-Credentials', "true");
-        $response->headers->set('Access-Control-Max-Age', "86400");
-        $response->headers->set('Access-Control-Allow-Headers', "Content-Type, Authorization, X-Requested-With");
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->headers->set('Access-Control-Max-Age', '86400');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 
         /*foreach($headers as $key => $value)
         {
