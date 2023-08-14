4515bf059bbb8b3fdf71bcf0363a40a19e9508fc
Pol Ribas <polribas14@gmail.com>
2023-08-14 12:31:40 +0200

    AbstractPrecalculatedModel

---
+++ src/AbstractPrecalculatedModel.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Bisual\LaravelShortcuts;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+/**
+ * HOW TO USE ABSTRACTPRECALCULATEDMODEL
+ *
+ *      1. Create a folder for AbstractPrecalculatedModel implementations (for example: "Models/Precalculated")
+ *      2. Create a specific-process implementation (for example: EcommerceTotalsLast30DaysPrecalculatedModel.php)
+ *          2.1. Implement refresh(), add() and $BASE_KEY
+ *      3. Create a kernel background process that executes refresh() method of each implementation
+ *      4. Create an async structure (Async Job or Event) that dispatches
+ *      5. Add the execution of a Dispatchable Job created in step 4 on Business Logic Layer (such as Repository or Service)
+ */
+abstract class AbstractPrecalculatedModel {
+
+    // la template de la key sin parámetros aplicados. Las variables serán envueltas entre doble claudators: {{var}} . Ejemplo: "ecommerce_total_last_30_days_branch_{{branch_id}}"
+    protected static string $BASE_KEY_TEMPLATE;
+
+    // This is the key with the parameters replaced
+    private string $base_key;
+
+    // We save $params for query purposes
+    protected array $params;
+
+    public final function __construct(array $params) {
+        $this->params = $params;
+        $this->base_key = $this->createKey($params);
+    }
+
+    /**
+     * ABSTRACT METHODS TO IMPLEMENT
+     */
+
+    // Refreshes the whole model
+    abstract public function refresh();
+
+    // Adds a new iteration without having to refresh the whole model
+    abstract public function add(array $array);
+
+    /**
+     * PUBLIC METHODS
+     */
+    final public function get(): array {
+        if(!$this->check()) $this->refresh();
+
+        return json_decode(Cache::get($this->getDataKey()), true);
+    }
+
+    final public function check(): bool {
+        return Cache::has($this->getDataKey());
+    }
+
+    final public function getUpdatedAt(): Carbon {
+        return Carbon::createFromTimestamp(Cache::get($this->getUpdatedAtKey()));
+    }
+
+    /**
+     * PROTECTED METHODS
+     */
+    final protected function set(array $data): void {
+        Cache::set($this->getDataKey(), json_encode($data));
+        Cache::set($this->getUpdatedAtKey(), Carbon::now()->timestamp);
+    }
+
+    final protected function generateDaysArray(Carbon $from, Carbon $to) {
+        $res = [];
+        $it = $from->copy();
+        while($it <= $to) {
+            $res[] = 0;
+            $it->addDay();
+        }
+
+        return $res;
+    }
+
+    /**
+     * PRIVATE METHODS
+     */
+    final private function getDataKey() { return $this->base_key . "_data"; }
+
+    final private function getUpdatedAtKey() { return $this->base_key . "_updated_at"; }
+
+    final static private function createKey(array $params) {
+        $res = self::$BASE_KEY_TEMPLATE;
+        foreach($params as $key => $val) {
+            $res = str_replace("{{$key}}", $val, $res);
+        }
+        return $res;
+    }
+
+}
